f93cb4d5e40bbe6498f6e15c09726a65ca00c2e6
naybyal <officiallynabiel@gmail.com>
2023-12-21 13:57:57 +0530

    Version 1.0

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 audioElement.play();
+
+if (navigator.getAutoplayPolicy(video) === "allowed") {
+    // The video element will autoplay with audio.
+} else if (navigator.getAutoplayPolicy(video) === "allowed-muted") {
+    // Mute audio on video
+    video.muted = true;
+} else if (navigator.getAutoplayPolicy(video) === "disallowed") {
+    // Set a default placeholder image.
+    video.poster = "https://w0.peakpx.com/wallpaper/490/440/HD-wallpaper-emerald-ring-jewelry-gem-black-feathers-white-gold.jpg";
+}
